325deeb22130d1f0899ce6913af329b8e6235381
nondanee <iminezn5656@outlook.com>
2020-06-22 00:09:13 +0800

    optimize requirements parsing (refer to zdict/zdict@b2edc12)

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@
 import os
 from setuptools import setup, find_packages
 
-with open(os.path.dirname(__file__) + '/requirements.txt') as reqs_file:
-    requirements = reqs_file.read().splitlines()
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    lines = [line.strip() for line in f]
+    requirements = [line for line in lines if line and not line.startswith('#')]
 
 setup(
     name = 'ncmdump',
