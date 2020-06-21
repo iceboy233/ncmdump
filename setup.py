e7a295092f13e9152b2e2802f113f0515d98dfe8
YujiaCheng1996 <YujiaCheng1996@users.noreply.github.com>
2020-06-22 00:02:31 +0800

    Fix AttributeError (#27)
    AttributeError: 'ParsedRequirement' object has no attribute 'req'
    https://github.com/pypa/pip/issues/8188

--- setup.py
+++ setup.py
@@ -2,15 +2,8 @@
 import os
 from setuptools import setup, find_packages
 
-try:
-    from pip._internal.req import parse_requirements
-except ImportError:
-    from pip.req import parse_requirements
-
-requirements = parse_requirements(
-    os.path.join(os.path.dirname(__file__), 'requirements.txt'),
-    session = False
-)
+with open(os.path.dirname(__file__) + '/requirements.txt') as reqs_file:
+    requirements = reqs_file.read().splitlines()
 
 setup(
     name = 'ncmdump',
@@ -25,7 +18,7 @@ setup(
     platforms = 'any',
     zip_safe = False,
     python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires = [str(requirement.req) for requirement in requirements],
+    install_requires = requirements,
     entry_points = {
         'console_scripts': [
             'ncmdump=ncmdump.app:main'
